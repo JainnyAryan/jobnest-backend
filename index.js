b91a096e58d7c9aebdbf40900c63fc553d66d3c2
Shravani Patil <shravani19092003@gmail.com>
2023-11-10 01:47:42 +0530

    auth

--- index.js
+++ index.js
@@ -20,11 +20,25 @@ app.post("/register", (req, res) => {
     .then((register) => res.json(register))
     .catch((err) => res.json(err));
 });
-app.post("/login", (req, res) => {
-  UserModel.findOne(req.body)
-    .then((register) => res.json(register))
-    .catch((err) => res.json(err));
-});
+app.post('/login',(req,res)=>{
+  const {email,password}=req.body;
+  UserModel.findOne({email:email})
+  .then(user =>{
+      if(user){
+          if(user.password===password)
+          {
+              res.json("Success")
+          }
+          else
+          {
+              res.json("the password is incorrect")
+          }
+      }
+      else{
+          res.json("No record existed");
+      }
+  })
+})
 
 app.listen(3001, () => {
   console.log("Server is running at 3001");
